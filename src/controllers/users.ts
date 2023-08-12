35393d05f197a5857dc6cb051b902cd35089401e
Alexander Davis <alexander.davis.098@gmail.com>
2023-08-12 17:17:59 -0300

    refactor: delete user controller

--- src/controllers/users.ts
+++
@@ -1,9 +0,0 @@
-import { Response } from 'express';
-
-import Users from '../models/users';
-
-export const usersCount = async (_: any, res: Response) => {
-    const users = await Users.find();
-    
-    return res.status(200).json({ users: users.length });
-};
