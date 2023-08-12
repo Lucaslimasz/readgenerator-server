acd823e2483ada5d6a1f1594379ec0904605f71a
Alexander Davis <alexander.davis.098@gmail.com>
2023-08-12 16:27:53 -0300

    refactor: fixing the user controller to return the quantity

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,8 +2,8 @@ import { Response } from 'express';
 
 import Users from '../models/users';
 
-export const usersCount = (_: any, res: Response) => {
-    const users = Users.count();
-
-    return res.status(200).json({ users: users });
+export const usersCount = async (_: any, res: Response) => {
+    const users = await Users.find();
+    
+    return res.status(200).json({ users: users.length });
 };
